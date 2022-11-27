d15d40260b55d09dcc2d348c52dbca106d0c6803
JustinaKTU <justina.laskovaite-koliniene@ktu.edu>
2022-11-27 17:44:15 +0200

    Updated data_transformation.R by fullfiling file

--- project/3-R/data_transformation.R
+++ project/3-R/data_transformation.R
@@ -1,3 +1,24 @@
-library(readr)
-read_csv("../1-data/put_your_data_files_here.csv")
-### save combined file into 1-data directory
+#install.packages("tidyverse")
+
+library(tidyverse)
+
+data <- read_csv("C:/Users/Justina/Desktop/DVDA_magistras/I_kursas/LAB5/1-sample_data.csv")
+
+select(data, y)
+
+data %>%
+  select(y)
+
+additional_features <- read_csv("C:/Users/Justina/Desktop/DVDA_magistras/I_kursas/LAB5/3-additional_features.csv")
+
+additional_data <- read_csv("C:/Users/Justina/Desktop/DVDA_magistras/I_kursas/LAB5/2-additional_data.csv")
+
+joined_data1 <- union_all(data, additional_data)
+joined_data2 <- inner_join(joined_data1, additional_features, by = "id")
+
+
+joined_data2 <- data %>%
+  inner_join(additional_features, by = "id")
+
+
+write_csv(joined_data2, "C:/Users/Justina/Desktop/DVDA_magistras/I_kursas/LAB5/KTU-DVDA-PROJECT/project/1-data/train_data.csv")  
