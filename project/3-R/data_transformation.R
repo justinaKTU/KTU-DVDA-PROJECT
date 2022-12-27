9e5bd23e5ddf3700c96495ad8a827926bc77cf34
JustinaKTU <justina.laskovaite-koliniene@ktu.edu>
2022-12-27 20:36:33 +0200

    Path updated

--- project/3-R/data_transformation.R
+++ project/3-R/data_transformation.R
@@ -1,19 +1,16 @@
 library(tidyverse)
 
-data <- read_csv("project/1-data/1-sample_data.csv")
+data <- read_csv("../1-data/1-sample_data.csv")
 
 select(data, y)
 
 data %>%
   select(y)
 
-additional_features <- read_csv("project/1-data/3-additional_features.csv")
-additional_data <- read_csv("project/1-data/2-additional_data.csv")
+additional_features <- read_csv("../1-data/3-additional_features.csv")
+additional_data <- read_csv("../1-data/2-additional_data.csv")
 
 joined_data1 <- union_all(data, additional_data)
 joined_data2 <- inner_join(joined_data1, additional_features, by = "id")
 
-joined_data2 <- data %>%
-  inner_join(additional_features, by = "id")
-
-write_csv(joined_data2, "project/1-data/train_data.csv")
+write_csv(joined_data2, "../1-data/train_data.csv")
